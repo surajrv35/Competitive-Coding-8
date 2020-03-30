ff2ea37e248e547cd8e1d5aa9aa7e2f41c8d85f8
Suraj Raghavendra Vadvadgi <45305881+surajrv35@users.noreply.github.com>
2020-03-30 17:22:53 -0500

    Create flatten-binary-tree-to-linked-list.java

---
+++ flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,36 @@
+// Time Complexity : O(N)
+// Space Complexity : O(N) for recursive stack space
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null)
+            return ;
+        
+        if(root.left != null) {
+            flatten(root.left);
+            TreeNode temp=root.right;
+            root.right=root.left;
+            root.left=null;
+            TreeNode curr=root.right;
+            while(curr.right != null)
+                curr=curr.right;
+            curr.right=temp;
+        }
+        
+        flatten(root.right);
+    }
+}
+
